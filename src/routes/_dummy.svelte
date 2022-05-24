5ede55d2901e3dbbbb6337da0815eb14e9338e61
Amr <akkk33@protonmail.com>
2022-05-24 09:02:08 +0200

    Using separate outline pass

--- src/routes/_dummy.svelte
+++ src/routes/_dummy.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
-  import { GLTF } from "threlte";
-  import { useGltfAnimations } from "$lib/threlte/useGltfAnimations";
+  import { browser } from "$app/env";
+  import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
+  import { Vector2, type Group } from "three";
+
+  import { GLTF, useThrelte, Pass } from "threlte";
+  import { useGltfAnimations } from "threlte/extras";
+
+  const { scene, camera } = useThrelte();
+
+  let width = (browser && window.innerWidth) || 1920;
+  let height = (browser && window.innerHeight) || 1080;
+
+  const outlinePass = new OutlinePass(
+    new Vector2(width, height),
+    scene,
+    $camera
+  );
+  outlinePass.edgeStrength = 5;
+  outlinePass.enabled = false;
 
   const { gltf, actions } = useGltfAnimations<"Confused">(({ actions }) => {
     // Either play your animations as soon as they are loaded
@@ -8,16 +25,25 @@
     // console.log(actions);
   });
 
+  let dummyModel: Group;
+  $: if (dummyModel) {
+    outlinePass.selectedObjects = [dummyModel];
+  }
+
   // Or play them whenever you need
   const triggerAnimation = () => {
-    console.log("click");
-    // actions["Take 001"]?.play();
+    $actions["Take 001"]?.play();
+    setTimeout(() => {
+      $actions["Take 001"]?.stop();
+    }, $actions["Take 001"].getClip().duration * 1000);
   };
   const pointerenter = () => {
     document.body.style.cursor = "pointer";
+    outlinePass.enabled = true;
   };
   const pointerleave = () => {
     document.body.style.cursor = "auto";
+    outlinePass.enabled = false;
   };
 </script>
 
@@ -30,8 +56,10 @@
   castShadow
   receiveShadow
   interactive
+  bind:scene={dummyModel}
   bind:gltf={$gltf}
   on:click={triggerAnimation}
   on:pointerenter={pointerenter}
   on:pointerleave={pointerleave}
 />
+<Pass pass={outlinePass} />
--- src/routes/_path.svelte
+++ src/routes/_path.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
-  import { GLTF, Group, Mesh } from "threlte";
-  import { useGltfAnimations } from "$lib/threlte/useGltfAnimations";
+  import { GLTF, Group, Mesh, Pass, useThrelte } from "threlte";
+  import { useGltfAnimations } from "threlte/extras";
   import type { Group as THREEGroup, Object3D, SkinnedMesh, Bone } from "three";
+  import { Vector2, BoxBufferGeometry, MeshBasicMaterial } from "three";
+  import { browser } from "$app/env";
+  import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
 
-  let frameVisible = false;
-  let frameObjectArray: (THREEGroup | Object3D | SkinnedMesh | Bone)[] = [];
+  const { scene, camera } = useThrelte();
 
-  let scene: THREEGroup;
+  let width = (browser && window.innerWidth) || 1920;
+  let height = (browser && window.innerHeight) || 1080;
 
-  $: if (scene) {
+  const outlinePass = new OutlinePass(
+    new Vector2(width, height),
+    scene,
+    $camera
+  );
+  outlinePass.edgeStrength = 5;
+  outlinePass.enabled = false;
+
+  let pathModel: THREEGroup;
+
+  $: if (pathModel) {
+    outlinePass.selectedObjects = [pathModel];
     // console.log(
     //   scene.children[0].children[0].children[0].children[0].children[0].children
     // );
-    scene.traverse((child) => {
-      frameObjectArray.push(child);
-      console.log(child.type);
-    });
-    frameVisible = true;
+    // pathModel.traverse((child) => {
+    //   frameObjectArray.push(child);
+    //   console.log(child.type);
+    // });
   }
 
   const { gltf, actions } = useGltfAnimations<"Confused">(({ actions }) => {
@@ -32,9 +45,11 @@
   };
   const pointerenter = () => {
     document.body.style.cursor = "pointer";
+    outlinePass.enabled = true;
   };
   const pointerleave = () => {
     document.body.style.cursor = "auto";
+    outlinePass.enabled = false;
   };
 </script>
 
@@ -48,20 +63,35 @@
     castShadow
     receiveShadow
     bind:gltf={$gltf}
-    bind:scene
+    bind:scene={pathModel}
+    on:click={triggerAnimation}
+    on:pointerenter={pointerenter}
+    on:pointerleave={pointerleave}
   />
-  {#if frameVisible}
-    {#each frameObjectArray as _obj}
-      <Mesh
-        interactive
-        position={{ z: -2 }}
-        rotation={{ x: Math.PI / 2, z: Math.PI / -2 }}
-        geometry={_obj.geometry}
-        material={_obj.material}
-        on:click={triggerAnimation}
-        on:pointerenter={pointerenter}
-        on:pointerleave={pointerleave}
-      />
-    {/each}
-  {/if}
+  <Mesh
+    interactive
+    position={{ y: 1 }}
+    on:click={triggerAnimation}
+    on:pointerenter={pointerenter}
+    on:pointerleave={pointerleave}
+    geometry={new BoxBufferGeometry(0.6, 2, 0.7)}
+    material={new MeshBasicMaterial()}
+    visible={false}
+  />
 </Group>
+<!-- {#if frameVisible}
+  {#each frameObjectArray as _obj}
+    <Mesh
+      interactive
+      position={{ z: -2 }}
+      rotation={{ x: Math.PI / 2, z: Math.PI / -2 }}
+      geometry={_obj.geometry}
+      material={_obj.material}
+      on:click={triggerAnimation}
+      on:pointerenter={pointerenter}
+      on:pointerleave={pointerleave}
+    />
+  {/each}
+{/if} -->
+
+<Pass pass={outlinePass} />
